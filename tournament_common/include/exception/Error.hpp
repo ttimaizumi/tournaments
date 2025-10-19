c65922d7bd19d0249bf641010b6716b0593db899
andres-go <ojeda.g.andres@gmail.com>
2025-10-19 22:58:57 +0000

    Error class with enum

---
+++ tournament_common/include/exception/Error.hpp
@@ -0,0 +1,15 @@
+//
+// Created by root on 10/19/25.
+//
+
+#ifndef TOURNAMENTS_ERROR_HPP
+#define TOURNAMENTS_ERROR_HPP
+
+enum class Error
+{
+    NOT_FOUND,
+    DUPLICATE,
+    INVALID_FORMAT
+};
+
+#endif //TOURNAMENTS_ERROR_HPP
