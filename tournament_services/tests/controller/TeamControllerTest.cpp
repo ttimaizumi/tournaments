7fbbb4ad8326b8a7da781bdb01eec0e9d5182fa2
RenataSz4 <renatasan0411@gmail.com>
2025-10-20 04:24:13 +0000

    update: TeamControllerTest

--- tournament_services/tests/controller/TeamControllerTest.cpp
+++ tournament_services/tests/controller/TeamControllerTest.cpp
@@ -7,184 +7,237 @@
 #include "controller/TeamController.hpp"
 
 class TeamDelegateMock : public ITeamDelegate {
-    public:
+public:
     MOCK_METHOD(std::shared_ptr<domain::Team>, GetTeam, (const std::string_view id), (override));
     MOCK_METHOD(std::vector<std::shared_ptr<domain::Team>>, GetAllTeams, (), (override));
     MOCK_METHOD(std::string_view, SaveTeam, (const domain::Team&), (override));
     MOCK_METHOD(std::string_view, UpdateTeam, (const domain::Team&), (override));
 };
 
-class TeamControllerTest : public ::testing::Test{
+class TeamControllerTest : public ::testing::Test {
 protected:
     std::shared_ptr<TeamDelegateMock> teamDelegateMock;
     std::shared_ptr<TeamController> teamController;
 
     void SetUp() override {
         teamDelegateMock = std::make_shared<TeamDelegateMock>();
-        teamController = std::make_shared<TeamController>(TeamController(teamDelegateMock));
+        teamController = std::make_shared<TeamController>(teamDelegateMock);
     }
 
-    // TearDown() function
     void TearDown() override {
-        // teardown code comes here
-    }
 
+    }
 };
 
-TEST_F(TeamControllerTest, GetTeamById_ErrorFormat) {
-    crow::response badRequest = teamController->getTeam("");
+// get team by id tests
 
-    EXPECT_EQ(badRequest.code, crow::BAD_REQUEST);
+TEST_F(TeamControllerTest, GetTeamById_InvalidFormat_ReturnsBadRequest) {
+    crow::response response = teamController->getTeam("");
+    EXPECT_EQ(crow::BAD_REQUEST, response.code);
 
-    badRequest = teamController->getTeam("mfasd#*");
-    EXPECT_EQ(badRequest.code, crow::BAD_REQUEST);
+    response = teamController->getTeam("mfasd#*");
+    EXPECT_EQ(crow::BAD_REQUEST, response.code);
+
+    response = teamController->getTeam("invalid id");
+    EXPECT_EQ(crow::BAD_REQUEST, response.code);
 }
 
-TEST_F(TeamControllerTest, GetTeamById) {
-
-    std::shared_ptr<domain::Team> expectedTeam = std::make_shared<domain::Team>(domain::Team{"my-id",  "Team Name"});
+TEST_F(TeamControllerTest, GetTeamById_ValidId_Returns200WithTeam) {
+    std::shared_ptr<domain::Team> expectedTeam =
+            std::make_shared<domain::Team>(domain::Team{"my-id", "Team Name"});
 
     EXPECT_CALL(*teamDelegateMock, GetTeam(testing::Eq(std::string("my-id"))))
-        .WillOnce(testing::Return(expectedTeam));
+            .WillOnce(testing::Return(expectedTeam));
 
     crow::response response = teamController->getTeam("my-id");
-    auto jsonResponse = crow::json::load(response.body);
+    auto jsonResponse = nlohmann::json::parse(response.body);
 
     EXPECT_EQ(crow::OK, response.code);
-    EXPECT_EQ(expectedTeam->Id, jsonResponse["id"]);
-    EXPECT_EQ(expectedTeam->Name, jsonResponse["name"]);
+    EXPECT_EQ(expectedTeam->Id, jsonResponse["id"].get<std::string>());
+    EXPECT_EQ(expectedTeam->Name, jsonResponse["name"].get<std::string>());
+    EXPECT_EQ("application/json", response.get_header_value("content-type"));
 }
 
-TEST_F(TeamControllerTest, GetTeamNotFound) {
-    EXPECT_CALL(*teamDelegateMock, GetTeam(testing::Eq(std::string("my-id"))))
-        .WillOnce(testing::Return(nullptr));
+TEST_F(TeamControllerTest, GetTeamById_NotFound_Returns404) {
+    EXPECT_CALL(*teamDelegateMock, GetTeam(testing::Eq(std::string("non-existent-id"))))
+            .WillOnce(testing::Return(nullptr));
 
-    crow::response response = teamController->getTeam("my-id");
+    crow::response response = teamController->getTeam("non-existent-id");
 
     EXPECT_EQ(crow::NOT_FOUND, response.code);
 }
 
-TEST_F(TeamControllerTest, SaveTeamTest) {
-    domain::Team capturedTeam;
-    EXPECT_CALL(*teamDelegateMock, SaveTeam(::testing::_))
-        .WillOnce(testing::DoAll(
-                testing::SaveArg<0>(&capturedTeam),
-                testing::Return("new-id")
-            )
-        );
+// get all team tests
 
-    nlohmann::json teamRequestBody = {{"name", "new team"}};
-    crow::request teamRequest;
-    teamRequest.body = teamRequestBody.dump();
-
-    crow::response response = teamController->SaveTeam(teamRequest);
-
-    testing::Mock::VerifyAndClearExpectations(&teamDelegateMock);
-
-    EXPECT_EQ(crow::CREATED, response.code);
-    EXPECT_EQ(teamRequestBody.at("name").get<std::string>(), capturedTeam.Name);
-}
-
-TEST_F(TeamControllerTest, SaveTeamTest_Conflict) {
-    domain::Team capturedTeam;
-    EXPECT_CALL(*teamDelegateMock, SaveTeam(::testing::_))
-        .WillOnce(testing::DoAll(
-                testing::SaveArg<0>(&capturedTeam),
-                testing::Return("")
-            )
-        );
-
-    nlohmann::json teamRequestBody = {{"name", "existing team"}};
-    crow::request teamRequest;
-    teamRequest.body = teamRequestBody.dump();
-
-    crow::response response = teamController->SaveTeam(teamRequest);
-
-    testing::Mock::VerifyAndClearExpectations(&teamDelegateMock);
-
-    EXPECT_EQ(crow::CONFLICT, response.code);
-    EXPECT_EQ(teamRequestBody.at("name").get<std::string>(), capturedTeam.Name);
-}
-
-TEST_F(TeamControllerTest, UpdateTeamTest) {
-    domain::Team capturedTeam;
-    EXPECT_CALL(*teamDelegateMock, UpdateTeam(::testing::_))
-        .WillOnce(testing::DoAll(
-                testing::SaveArg<0>(&capturedTeam),
-                testing::Return("updated-id")
-            )
-        );
-
-    nlohmann::json teamRequestBody = {{"name", "updated team"}};
-    crow::request teamRequest;
-    teamRequest.body = teamRequestBody.dump();
-
-    crow::response response = teamController->UpdateTeam(teamRequest, "updated-id");
-
-    testing::Mock::VerifyAndClearExpectations(&teamDelegateMock);
-
-    EXPECT_EQ(crow::NO_CONTENT, response.code);
-    EXPECT_EQ("updated-id", capturedTeam.Id);
-    EXPECT_EQ(teamRequestBody.at("name").get<std::string>(), capturedTeam.Name);
-}
-
-TEST_F(TeamControllerTest, UpdateTeamTest_NotFound) {
-    domain::Team capturedTeam;
-    EXPECT_CALL(*teamDelegateMock, UpdateTeam(::testing::_))
-        .WillOnce(testing::DoAll(
-                testing::SaveArg<0>(&capturedTeam),
-                testing::Return("")
-            )
-        );
-
-    nlohmann::json teamRequestBody = {{"name", "updated team"}};
-    crow::request teamRequest;
-    teamRequest.body = teamRequestBody.dump();
-
-    crow::response response = teamController->UpdateTeam(teamRequest, "non-existent-id");
-
-    testing::Mock::VerifyAndClearExpectations(&teamDelegateMock);
-
-    EXPECT_EQ(crow::NOT_FOUND, response.code);
-    EXPECT_EQ("non-existent-id", capturedTeam.Id);
-    EXPECT_EQ(teamRequestBody.at("name").get<std::string>(), capturedTeam.Name);
-}
-// busca todos equipos con lista de objetos HTTP 200
-TEST_F(TeamControllerTest, GetAllTeams_WithTeams_Returns200) {
+TEST_F(TeamControllerTest, GetAllTeams_WithMultipleTeams_Returns200WithArray) {
     std::vector<std::shared_ptr<domain::Team>> teams = {
-        std::make_shared<domain::Team>(domain::Team{"id-1", "Team 1"}),
-        std::make_shared<domain::Team>(domain::Team{"id-2", "Team 2"})
+            std::make_shared<domain::Team>(domain::Team{"id-1", "Team 1"}),
+            std::make_shared<domain::Team>(domain::Team{"id-2", "Team 2"}),
+            std::make_shared<domain::Team>(domain::Team{"id-3", "Team 3"})
     };
 
     EXPECT_CALL(*teamDelegateMock, GetAllTeams())
-        .WillOnce(testing::Return(teams));
+            .WillOnce(testing::Return(teams));
 
     crow::response response = teamController->getAllTeams();
+    auto jsonResponse = nlohmann::json::parse(response.body);
 
     EXPECT_EQ(crow::OK, response.code);
-    auto jsonResponse = crow::json::load(response.body);
-    EXPECT_EQ(2, jsonResponse.size());
+    EXPECT_EQ(3, jsonResponse.size());
+    EXPECT_EQ("id-1", jsonResponse[0]["id"].get<std::string>());
+    EXPECT_EQ("Team 1", jsonResponse[0]["name"].get<std::string>());
+    EXPECT_EQ("id-2", jsonResponse[1]["id"].get<std::string>());
+    EXPECT_EQ("Team 2", jsonResponse[1]["name"].get<std::string>());
+    EXPECT_EQ("application/json", response.get_header_value("content-type"));
 }
 
-//busca todos equipos con lista vacia HTTP 200
-TEST_F(TeamControllerTest, GetAllTeams_EmptyList_Returns200) {
+TEST_F(TeamControllerTest, GetAllTeams_EmptyList_Returns200WithEmptyArray) {
     std::vector<std::shared_ptr<domain::Team>> emptyTeams;
 
     EXPECT_CALL(*teamDelegateMock, GetAllTeams())
-        .WillOnce(testing::Return(emptyTeams));
+            .WillOnce(testing::Return(emptyTeams));
 
     crow::response response = teamController->getAllTeams();
+    auto jsonResponse = nlohmann::json::parse(response.body);
 
     EXPECT_EQ(crow::OK, response.code);
-    auto jsonResponse = crow::json::load(response.body);
     EXPECT_EQ(0, jsonResponse.size());
+    EXPECT_TRUE(jsonResponse.is_array());
+    EXPECT_EQ("application/json", response.get_header_value("content-type"));
 }
 
-TEST_F(TeamControllerTest, UpdateTeam_ErrorFormat) {
-    crow::response badRequest = teamController->UpdateTeam(crow::request{}, "");
+// save team tests
 
-    EXPECT_EQ(badRequest.code, crow::BAD_REQUEST);
+TEST_F(TeamControllerTest, SaveTeam_ValidRequest_Returns201WithLocation) {
+    domain::Team capturedTeam;
+    const std::string expectedId = "generated-id-12345";
 
-    badRequest = teamController->UpdateTeam(crow::request{}, "mfasd#*");
-    EXPECT_EQ(badRequest.code, crow::BAD_REQUEST);
+    EXPECT_CALL(*teamDelegateMock, SaveTeam(testing::_))
+            .WillOnce(testing::DoAll(
+                    testing::SaveArg<0>(&capturedTeam),
+                    testing::Return(expectedId)
+            ));
+
+    nlohmann::json teamRequestBody = {{"name", "New Team"}};
+    crow::request teamRequest;
+    teamRequest.body = teamRequestBody.dump();
+
+    crow::response response = teamController->SaveTeam(teamRequest);
+
+    EXPECT_EQ(crow::CREATED, response.code);
+    EXPECT_EQ("New Team", capturedTeam.Name);
+    EXPECT_EQ(expectedId, response.get_header_value("location"));
+
+    testing::Mock::VerifyAndClearExpectations(teamDelegateMock.get());
+}
+
+TEST_F(TeamControllerTest, SaveTeam_DuplicateName_Returns409Conflict) {
+    domain::Team capturedTeam;
+
+    EXPECT_CALL(*teamDelegateMock, SaveTeam(testing::_))
+            .WillOnce(testing::DoAll(
+                    testing::SaveArg<0>(&capturedTeam),
+                    testing::Return("")
+            ));
+
+    nlohmann::json teamRequestBody = {{"name", "Existing Team"}};
+    crow::request teamRequest;
+    teamRequest.body = teamRequestBody.dump();
+
+    crow::response response = teamController->SaveTeam(teamRequest);
+
+    EXPECT_EQ(crow::CONFLICT, response.code);
+    EXPECT_EQ("Existing Team", capturedTeam.Name);
+
+    testing::Mock::VerifyAndClearExpectations(teamDelegateMock.get());
+}
+
+TEST_F(TeamControllerTest, SaveTeam_EmptyBody_ReturnsBadRequest) {
+    crow::request teamRequest;
+    teamRequest.body = "";
+
+    crow::response response = teamController->SaveTeam(teamRequest);
+
+    EXPECT_EQ(crow::BAD_REQUEST, response.code);
+}
+
+// update team tests
+
+TEST_F(TeamControllerTest, UpdateTeam_ValidRequest_Returns204NoContent) {
+    domain::Team capturedTeam;
+    const std::string teamId = "existing-id-123";
+
+    EXPECT_CALL(*teamDelegateMock, UpdateTeam(testing::_))
+            .WillOnce(testing::DoAll(
+                    testing::SaveArg<0>(&capturedTeam),
+                    testing::Return(teamId)
+            ));
+
+    nlohmann::json teamRequestBody = {{"name", "Updated Team Name"}};
+    crow::request teamRequest;
+    teamRequest.body = teamRequestBody.dump();
+
+    crow::response response = teamController->UpdateTeam(teamRequest, teamId);
+
+    EXPECT_EQ(crow::NO_CONTENT, response.code);
+    EXPECT_EQ(teamId, capturedTeam.Id);
+    EXPECT_EQ("Updated Team Name", capturedTeam.Name);
+
+    testing::Mock::VerifyAndClearExpectations(teamDelegateMock.get());
+}
+
+TEST_F(TeamControllerTest, UpdateTeam_TeamNotFound_Returns404) {
+    domain::Team capturedTeam;
+    const std::string nonExistentId = "non-existent-id";
+
+    EXPECT_CALL(*teamDelegateMock, UpdateTeam(testing::_))
+            .WillOnce(testing::DoAll(
+                    testing::SaveArg<0>(&capturedTeam),
+                    testing::Return("") // Empty string indica que no existe
+            ));
+
+    nlohmann::json teamRequestBody = {{"name", "Updated Team"}};
+    crow::request teamRequest;
+    teamRequest.body = teamRequestBody.dump();
+
+    crow::response response = teamController->UpdateTeam(teamRequest, nonExistentId);
+
+    EXPECT_EQ(crow::NOT_FOUND, response.code);
+    EXPECT_EQ(nonExistentId, capturedTeam.Id);
+    EXPECT_EQ("Updated Team", capturedTeam.Name);
+
+    testing::Mock::VerifyAndClearExpectations(teamDelegateMock.get());
+}
+
+TEST_F(TeamControllerTest, UpdateTeam_InvalidIdFormat_ReturnsBadRequest) {
+    crow::request teamRequest;
+    nlohmann::json teamRequestBody = {{"name", "Team"}};
+    teamRequest.body = teamRequestBody.dump();
+
+    crow::response response = teamController->UpdateTeam(teamRequest, "");
+    EXPECT_EQ(crow::BAD_REQUEST, response.code);
+
+    response = teamController->UpdateTeam(teamRequest, "invalid#id*");
+    EXPECT_EQ(crow::BAD_REQUEST, response.code);
+
+    response = teamController->UpdateTeam(teamRequest, "id with spaces");
+    EXPECT_EQ(crow::BAD_REQUEST, response.code);
+}
+
+TEST_F(TeamControllerTest, UpdateTeam_InvalidJSON_ReturnsBadRequest) {
+    crow::request teamRequest;
+    teamRequest.body = "not valid json";
+
+    crow::response response = teamController->UpdateTeam(teamRequest, "valid-id");
+
+    EXPECT_EQ(crow::BAD_REQUEST, response.code);
+}
+
+TEST_F(TeamControllerTest, UpdateTeam_EmptyBody_ReturnsBadRequest) {
+    crow::request teamRequest;
+    teamRequest.body = "";
+
+    crow::response response = teamController->UpdateTeam(teamRequest, "valid-id");
+
+    EXPECT_EQ(crow::BAD_REQUEST, response.code);
 }
