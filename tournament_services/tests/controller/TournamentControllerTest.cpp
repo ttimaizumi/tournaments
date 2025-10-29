b721cb79253d92acf6e337a46e876b3f6aa952cf
tomas <ttimaizumi@outlook.com>
2025-10-29 11:33:45 -0700

    Commented out invalid test

--- tournament_services/tests/controller/TournamentControllerTest.cpp
+++ tournament_services/tests/controller/TournamentControllerTest.cpp
@@ -4,13 +4,13 @@
 
 
 TEST(TournamentControllerTest, CreateTournament) {
-    // std::shared_ptr<TournamentController> tournamentController;
-    // tournamentController->ReadAll();
-    // std::string id = "ID";
-    // std::string name = "Name";
-    //
-    // domain::Team team = {id,name};
-    //
-    // EXPECT_EQ(team.Id.c_str(), id);
-    // EXPECT_EQ(team.Name.c_str(), name);
+    std::shared_ptr<TournamentController> tournamentController;
+    tournamentController->ReadAll();
+    std::string id = "ID";
+    std::string name = "Name";
+
+    domain::Team team = {id,name};
+
+    EXPECT_EQ(team.Id.c_str(), id);
+    EXPECT_EQ(team.Name.c_str(), name);
 }
