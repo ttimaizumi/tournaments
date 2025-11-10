74b121602250a8702b3c824ed62f34fd3334a5b6
RenataSz4 <renatasan0411@gmail.com>
2025-11-10 00:53:12 +0000

    add IMatchDelegate

---
+++ tournament_services/include/delegate/IMatchDelegate.hpp
@@ -0,0 +1,20 @@
+#ifndef IMATCH_DELEGATE_HPP
+#define IMATCH_DELEGATE_HPP
+
+#include <string_view>
+#include <string>
+#include <memory>
+#include <vector>
+#include <expected>
+
+#include "domain/Match.hpp"
+
+class IMatchDelegate {
+public:
+    virtual ~IMatchDelegate() = default;
+    virtual std::expected<std::shared_ptr<domain::Match>, std::string> GetMatch(std::string_view tournamentId, std::string_view matchId) = 0;
+    virtual std::expected<std::vector<std::shared_ptr<domain::Match>>, std::string> GetMatches(std::string_view tournamentId, std::string_view filter) = 0;
+    virtual std::expected<void, std::string> UpdateMatchScore(std::string_view tournamentId, std::string_view matchId, const domain::Score& score) = 0;
+};
+
+#endif /* ITEAM_DELEGATE_HPP */
